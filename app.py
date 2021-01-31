34b1d279de8294a2b0ff9ac560d185e9dc0d6d82
matiashauge <matiashauge99@gmail.com>
2021-01-31 17:11:50 +0100

    New changes to the generator

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import random, string
 # The sign that can be in a password
 
-password_signs = ['´', '@', '$', '#', '/' '()','<', '>']
+password_signs = ['´', '@', '$', '#', '/', '()', '<', '>']
 password_min_length = 5
 password_max_length = 32
 
@@ -9,7 +9,12 @@ lowercase_str = string.ascii_lowercase
 uppercase_str = string.ascii_uppercase
 all_letters = string.ascii_letters
 
-for letters in all_letters:
-    for signs in password_signs:
-        pass
+def string_allowed(pass_str_allowed):
 
+    for i in pass_str_allowed:
+        if i not in pass_str_allowed:
+            print('Its not allowed')
+        if i in pass_str_allowed:
+            print('Its allowed')
+
+string_allowed(password_signs)
