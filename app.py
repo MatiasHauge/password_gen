d6ad82bebeab20e05102f745e593c91f87d4d775
Matias Hauge <matiashauge@Matiass-MacBook-Pro.local>
2021-01-31 16:16:38 +0100

    new change

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
+import random, string
+# The sign that can be in a password
 
-# Password generator
+password_signs = ['´', '@', '$', '#', '/' '()','<', '>']
+password_min_length = 5
+password_max_length = 32
+
+lowercase_str = string.ascii_lowercase
+uppercase_str = string.ascii_uppercase
+all_letters = string.ascii_letters
+
+for letters in all_letters:
+    for signs in password_signs:
+        pass
+
